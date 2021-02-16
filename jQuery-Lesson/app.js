0f9b3ed628dad27c768fbf982942992169ad28d6
sumi-re01 <owob.viola@gmail.com>
2021-02-16 20:47:09 +0900

    [Finish]JavaScript 7章 this/children

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -98,5 +98,42 @@
 
 
 
-// ７章
+// // ７章
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $('.bg1').slideUp();
+//   });
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
 
+// // ↓thisを使って書き直す
+
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $(this).slideUp();
+//   });
+//   $('.bg2').on('click', function(){
+//     $(this).slideUp();
+//   });
+//   $('.bg3').on('click', function(){
+//     $(this).slideUp();
+//   });
+//   $('.bg4').on('click', function(){
+//     $(this).slideUp();
+//   });
+// });
+
+// children
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
+  });
+});
