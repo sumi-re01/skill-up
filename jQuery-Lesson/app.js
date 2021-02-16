6d31ac69e89053b8faaf2b904c0871b3731111a4
sumi-re01 <owob.viola@gmail.com>
2021-02-16 16:36:31 +0900

    [Finish]JavaScript 5章jQuery-css

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,50 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>')
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>')
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+
+// // スライドダウン
+// $(document).ready(function(){
+//   $('.box1').slideDown();
+// });
+
+
+// // スライドアップ
+// $(document).ready(function(){
+//   $('.box1').slideUp();
+// });
+
+// // 隠す
+// $(function(){
+//   $('.box1').hide();
+// });
+
+
+// 確認問題
+$(function(){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px',
+      'width': '200px'
+    }).slideUp();
+  });
 });
+
+// 回答
+// $(function (){
+//   $('.box1').slideDown(function (){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp();
+//   });
+// });
